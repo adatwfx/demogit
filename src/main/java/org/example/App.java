509f27b3d8e5f191a5322bccb5e424587ec89098
Anyujie <01566910@yto.net.cn>
2023-03-09 16:05:30 +0800

    te6

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -29,5 +29,6 @@ public class App
         System.out.println( "test3!" );
         System.out.println( "test4!" );
         System.out.println( "test5!" );
+        System.out.println( "test6!" );
     }
 }
