e7ff4bf6fb7bc78bc24a57877866551c08b43239
Anyujie <01566910@yto.net.cn>
2023-03-09 15:45:04 +0800

    ma1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
         System.out.println( "Hello World222!" );
         System.out.println( "Hello World2333333!" );
         System.out.println( "Hello World3333!" );
