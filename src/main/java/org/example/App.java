f319e605754a9ec80f5949043fc5a42625b5a38c
Anyujie <01566910@yto.net.cn>
2023-03-09 10:37:52 +0800

    hello55

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println( "Hello World2333333!" );
         System.out.println( "Hello World3333!" );
         System.out.println( "Hello World4444!" );
+        System.out.println( "Hello World5555!" );
     }
 }
