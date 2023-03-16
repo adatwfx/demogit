ef50efecdf870347fecb4ec32d73e216da382f9e
Anyujie <01566910@yto.net.cn>
2023-03-16 13:28:47 +0800

    test8

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -32,5 +32,6 @@ public class App
         System.out.println( "test5!" );
         System.out.println( "test6!" );
         System.out.println( "test7" );
+        System.out.println( "test8" );
     }
 }
