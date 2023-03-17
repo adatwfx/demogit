f8161da5a829d2a62f2a99f089e1166b539b2f37
Anyujie <01566910@yto.net.cn>
2023-03-17 13:27:34 +0800

    issue10-1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "issue10-1" );
         System.out.println( "C5" );
         System.out.println( "issue8" );
         System.out.println( "issue7" );
