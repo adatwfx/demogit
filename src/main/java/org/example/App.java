c93c38024ff11929ba948eaae92c88346ea408ce
Anyujie <01566910@yto.net.cn>
2023-03-17 11:14:18 +0800

    issue8 fixed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "issue8" );
         System.out.println( "issue7" );
         System.out.println( "issue6" );
         System.out.println( "issue5" );
